aaffe8c312b07bb5edd35065a8bb45c7aa44947f
Matt Iandoli <miandoli@gmail.com>
2018-03-20 15:04:22 -0400

    Initial File

---
+++ hitori.jl
@@ -0,0 +1,319 @@
+function printpuzzle(puzzle, solve, highX = 0, highY = 0)
+    for i = 1:size(puzzle, 1)
+        for j = 1:size(puzzle, 2)
+            curr = puzzle[i, j]
+            space = curr < 10 ? "    " : "   "
+            if solve[i, j] == "○"
+                if i == highX && j == highY
+                    print_with_color(:blue, curr, space)
+                else
+                    print_with_color(:green, curr, space)
+                end
+            elseif solve[i, j] == "■"
+                if i == highX && j == highY
+                    print_with_color(:blue, curr, space)
+                else
+                    bold = true;
+                    print_with_color(:red, curr, space)
+                end
+            else
+                if i == highX && j == highY
+                    print_with_color(:blue, curr, space)
+                else
+                    print(curr, space)
+                end
+            end
+        end
+        print("\n\n")
+    end
+end
+
+function solve(puzzle)
+
+    solution = fill("□", size(puzzle, 1), size(puzzle, 2))
+
+    function circle(x, y)
+        solution[x, y] = "○"
+        curr = puzzle[x, y]
+
+        # Checks down the column
+        for i = 1:size(puzzle, 1)
+            val = puzzle[i, y]
+            state = solution[i, y]
+            if state == "□" && i != x && val == curr
+                shade(i, y)
+            end
+        end
+
+        # Checks along the row
+        for j = 1:size(puzzle, 2)
+            val = puzzle[x, j]
+            state = solution[x, j]
+            if state == "□" && j != y && val == curr
+                shade(x, j)
+            end
+        end
+    end
+
+    function shade(x, y)
+        # Fill in square
+        solution[x, y] = "■"
+
+        # Circle adjacents
+        for i = (x - 1):(x + 1)
+            if i > 0 && i <= size(puzzle, 1) &&  i != x && solution[i, y] == "□"
+                circle(i, y)
+            end
+        end
+        for j = (y - 1):(y + 1)
+            if j > 0 && j <= size(puzzle, 2) && j != y && solution[x, j] == "□"
+                circle(x, j)
+            end
+        end
+    end
+
+    function singles()
+        # Goes thru all the rows
+        for i = 1:size(puzzle, 1)
+            # Goes thru each column in the row and checks if its the only one
+            for j = 1:size(puzzle, 2)
+                single = true
+                curr = puzzle[i, j]
+                if solution[i, j] == "□"
+                    # Checks along the column
+                    for k = 1:size(puzzle, 1)
+                        state = solution[k, j]
+                        val = puzzle[k, j]
+                        # Checks:   not itself
+                        #           not filled in
+                        #           same value
+                        if k != i && state != "■" && val == curr
+                            single = false
+                            break
+                        end
+                    end
+
+                    # Checks to see if it is still single
+                    if single
+                        # Checks along the row
+                        for k = 1:size(puzzle, 2)
+                            val = puzzle[i, k]
+                            state = solution[i, k]
+                            if k != j && state != "■" && val == curr
+                                sinlge = false
+                                break
+                            end
+                        end
+                    end
+
+                    # Checks if it is the only one in the row and column
+                    if single
+                        circle(i, j)
+                    end
+                end
+
+            end
+        end
+    end
+
+    function flooding()
+        changed = false;
+
+        history = fill(0, size(puzzle, 1), size(puzzle, 2))
+
+        function countLiberties(x, y)
+
+            history[x, y] = 1
+
+            function isLiberty(i, j)
+                if history[i, j] == "0"
+                    if puzzle[i, j] == "○"
+                        return countLiberties(i, j)
+                    elseif puzzle[i, j] == "□"
+                        return 1
+                    else
+                        return 0
+                    end
+                    history[i, j] = 1
+                end
+            end
+
+            libCount = 0
+
+            for i = (x - 1):(x + 1)
+                if i > 0 && i <= size(puzzle, 1)
+                    libCount += isLiberty(i, y)
+                end
+            end
+            for j = (y - 1):(y + 1)
+                if j > 0 && j <= size(puzzle, 2)
+                    libCount += isLiberty(x, j)
+                end
+            end
+            return libCount
+        end
+
+        circling = fill(0, size(puzzle, 1), size(puzzle, 2))
+
+        function circleLiberty(x, y)
+            circling[x, y] = 1
+            for i = (x - 1):(x + 1)
+                if i > 0 && i <= size(puzzle, 1)
+                    if circling[i, y] == 0
+                        if puzzle[i, y] == "○"
+                            circleLiberty(i, y)
+                        elseif puzzle[i, y] == "□"
+                            circle(i, y)
+                        end
+                    end
+                end
+            end
+            for j = (y - 1):(y + 1)
+                if j > 0 && j <= size(puzzle, 2)
+                    if circling[x, j] == 0
+                        if puzzle[x, j] == "○"
+                            circleLiberty(x, j)
+                        elseif puzzle[i, y] == "□"
+                            circle(x, j)
+                        end
+                    end
+                end
+            end
+        end
+
+        for i = 1:size(puzzle, 1)
+            for j = 1:size(puzzle, 2)
+                if puzzle[i, j] == "○"
+                    if countLiberties(i, j) == 1
+                        changed = true
+                        circleLiberty(i, j)
+                    end
+                end
+            end
+        end
+        return changed
+    end
+
+    function doubles()
+        for i = 1:(size(puzzle, 1) - 1)
+            for j = 1:(size(puzzle, 2) - 1)
+                #printpuzzle(puzzle, solution, i, j)
+                #readline(STDIN)
+
+                # Check doubles down the columns
+                if puzzle[i, j] == puzzle[i + 1, j]
+                    # Shade in any other occurences of the value in the column
+                    for k = 1:size(puzzle, 1)
+                        if solution[k, j] == "□" && k != i && k != i + 1 && puzzle[k, j] == puzzle[i, j]
+                            shade(k, j)
+                        end
+                    end
+                end
+
+                # Check doubles along the rows
+                if puzzle[i, j] == puzzle[i, j + 1]
+                    # Shade in any other occurences of the value in the row
+                    for k = 1:size(puzzle, 2)
+                        if solution[i, k] == "□" && k != j && k != j + 1 && puzzle[i, k] == puzzle[i, j]
+                            shade(i, k)
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    doubles()
+    singles()
+
+    #while flooding()
+    #end
+
+    printpuzzle(puzzle, solution)
+end
+
+saveChar = "□■○"
+
+#152
+puzzle152 = vcat([7 8 2 7 2 2 2 3],
+                [4 7 5 6 8 1 3 2],
+                [1 4 2 3 5 6 8 7],
+                [6 6 7 4 2 7 5 8],
+                [2 2 1 1 1 4 6 5],
+                [2 5 3 4 6 7 3 8],
+                [2 6 4 7 7 7 2 1],
+                [8 3 8 2 4 5 7 5])
+#161
+puzzle161 = vcat([3 7 8 8 6 5 2 2],
+                [8 8 5 1 6 6 2 2],
+                [7 2 8 2 2 2 6 5],
+                [6 2 7 3 1 8 5 2],
+                [5 1 6 2 6 7 5 2],
+                [5 5 5 6 4 4 8 7],
+                [2 2 3 7 5 4 1 3],
+                [8 4 1 1 3 3 7 6])
+#177
+puzzle177 = vcat([8 12 5 2 2 2 11 3 3 3],
+                [6 10 7 9 4 8 1 3 5 12],
+                [3 11 4 6 5 5 5 12 2 4],
+                [8 6 9 8 6 10 3 5 3 2],
+                [2 10 8 5 12 4 7 11 1 8],
+                [5 11 6 8 8 7 12 12 4 12],
+                [8 9 2 11 11 11 8 10 10 7],
+                [7 5 8 10 5 9 2 5 6 1],
+                [12 8 1 3 11 2 7 9 11 7],
+                [10 6 11 8 8 12 7 7 7 4],
+                [11 1 12 2 8 1 6 8 10 5],
+                [7 5 10 12 7 2 4 10 8 4])
+#193
+puzzle193 = vcat([3 4 12 11 13 6 13 14 5 5 2 1],
+                [9 3 11 12 12 12 14 13 1 6 1 14],
+                [11 9 5 4 4 14 10 7 13 15 14 9],
+                [15 1 9 6 10 3 12 4 2 12 14 13],
+                [2 5 13 12 3 7 1 15 6 8 14 2],
+                [6 4 15 4 1 1 3 4 8 12 5 4],
+                [12 13 6 5 7 15 9 8 9 1 11 14],
+                [7 5 1 15 2 10 5 3 9 7 6 5],
+                [13 15 8 11 9 6 11 10 9 3 13 12],
+                [5 3 14 13 12 8 14 1 7 9 5 3],
+                [10 11 11 3 5 13 7 12 12 13 9 9],
+                [14 2 10 9 11 9 13 12 12 5 15 14],
+                [9 10 3 6 4 12 14 5 11 6 8 9],
+                [13 11 7 6 8 1 10 6 15 2 12 11],
+                [7 9 2 6 3 7 5 4 2 12 7 2])
+#194
+puzzle194 = vcat([6 4 8 5 15 7 2 13 10 12 11 4],
+                [6 11 15 10 7 13 1 3 2 4 12 13],
+                [6 5 5 6 1 8 10 14 7 13 12 7],
+                [2 6 11 7 3 9 12 1 13 15 12 9],
+                [8 13 2 14 9 1 11 7 4 14 5 13],
+                [3 10 12 7 4 3 13 2 12 1 15 10],
+                [10 15 4 1 12 3 5 10 6 8 15 2],
+                [13 10 7 10 14 3 8 12 9 4 10 13],
+                [1 14 1 15 10 2 4 8 5 7 6 1],
+                [7 12 1 7 8 12 2 13 3 7 4 14],
+                [5 15 14 11 7 13 1 4 8 3 2 5],
+                [4 2 6 5 13 5 7 11 8 10 11 4],
+                [4 9 5 10 7 12 14 3 8 2 1 11],
+                [4 1 5 2 12 4 5 10 7 13 8 3],
+                [1 6 5 7 8 9 11 1 12 14 13 1])
+# Solve puzzle
+arg = 177
+if size(ARGS, 1) == 1
+    arg = ARGS[1]
+end
+
+println("Puzzle ", arg, "\n")
+puzzle = puzzle177
+if arg == 152
+    puzzle = puzzle152
+elseif arg == 161
+    puzzle = puzzle161
+elseif arg == 177
+    puzzle = puzzle177
+elseif arg == 193
+    puzzle = puzzle193
+elseif arg == 194
+    puzzle = puzzle194
+end
+solve(puzzle)
